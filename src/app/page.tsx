c2f8fd95663862d833af8fd3c2280e6361d27912
itsmemuqorrobin <achmadmuqorrobinassholih@gmail.com>
2026-04-18 14:12:55 +0700

    Preparing Project: Theme style adjustment.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <div>
-        <h1 className="text-2xl  ">testing</h1>
+      <div className="bg-blue-1 use--max-container-boundary w-full flex items-center gap-4">
+        <h1 className="text-2xl text-black-1  ">testing</h1>
       </div>
     </>
   );
