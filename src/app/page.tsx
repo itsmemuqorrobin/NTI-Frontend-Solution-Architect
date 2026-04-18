93e33b5130dcaa29c0499942db9463d77a204c1b
itsmemuqorrobin <achmadmuqorrobinassholih@gmail.com>
2026-04-18 13:50:20 +0700

    Preparing Project: Adjust fonts pattern and intregate it with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div>
-        <h1 className="text-2xl text-[#1D1D1F] font-normal">testing</h1>
+        <h1 className="text-2xl  ">testing</h1>
       </div>
     </>
   );
