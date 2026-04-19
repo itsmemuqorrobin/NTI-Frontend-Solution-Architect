7bb014c315a100106d27e293ddf110e072026e5e
itsmemuqorrobin <achmadmuqorrobinassholih@gmail.com>
2026-04-19 15:03:06 +0700

    Animation: Testing purposes

--- src/_ui/sections/Hero.tsx
+++ src/_ui/sections/Hero.tsx
@@ -1,28 +1,66 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
+import { useRef } from "react";
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
 
 export function Hero() {
-  return (
-    <section className="bg-grey-1 h-[calc(100vh-var(--spacing-size-of-navbar-to-main))] mt-size-of-navbar-to-main z-5 flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="use--font-sf-pro-display text-black-1 font-bold text-[123px]">
-          AirPods
-        </h1>
-        <h2 className="use--font-sf-pro-display text-black-1 font-bold text-[22px] -mt-[30px]">
-          3rd generation
-        </h2>
-        <p className=" use--font-sf-pro-display text-black-1 text-[22px] mt-1 ">
-          From ₹19900.00*
-        </p>
-      </div>
+  const sectionRef = useRef<HTMLElement | null>(null);
 
-      <div>
-        {/* <Image
+  useGSAP(
+    () => {
+      gsap.to(".box", {
+        scrollTrigger: {
+          trigger: ".box",
+          start: "top center",
+          markers: true,
+          scrub: true,
+          pin: true,
+        },
+        x: 500,
+        duration: 3,
+      });
+    },
+    { scope: sectionRef },
+  );
+
+  return (
+    <section ref={sectionRef}>
+      <section className="bg-grey-1 h-screen mt-size-of-navbar-to-main z-5 flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="use--font-sf-pro-display text-black-1 font-bold text-[123px]">
+            AirPods
+          </h1>
+          <h2 className="use--font-sf-pro-display text-black-1 font-bold text-[22px] -mt-[30px]">
+            3rd generation
+          </h2>
+          <p className=" use--font-sf-pro-display text-black-1 text-[22px] mt-1 ">
+            From ₹19900.00*
+          </p>
+        </div>
+
+        <div>
+          {/* <Image
           src={"/images/illustrations/hero_airpods_left.png"}
           alt="AirPods Right"
           width={500}
           height={500}
           className="absolute top-[314.96px] left-[1112.84px]"
         /> */}
+        </div>
+      </section>
+
+      <h1 className="box py-20 bg-blue-1 text-white w-20 px-20">Testing</h1>
+
+      <div className="h-screen mt-20">
+        <p className="text-center text-2xl font-bold">
+          Scroll down to see the animation
+        </p>
       </div>
     </section>
   );
