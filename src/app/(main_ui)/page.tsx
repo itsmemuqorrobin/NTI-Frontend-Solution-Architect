6f5b6894265615b2955c811451b4a191281d6e81
itsmemuqorrobin <achmadmuqorrobinassholih@gmail.com>
2026-04-23 12:05:06 +0700

    Performances: Code split on component level to chunk to reduce load from heavy bundle

--- src/app/(main_ui)/page.tsx
+++ src/app/(main_ui)/page.tsx
@@ -1,7 +1,19 @@
-import { AirpodsMaxSection } from "@/_ui/sections/AripodsMax";
-import { CompareAirpodsSection } from "@/_ui/sections/CompareAripods";
 import { HeroSection } from "@/_ui/sections/Hero";
 import { NavbarSection } from "@/_ui/sections/Navbar";
+
+import dynamic from "next/dynamic";
+
+const DynamicAirpodsMaxSection = dynamic(() =>
+  import("@/_ui/sections/AripodsMax").then(
+    (component) => component.AirpodsMaxSection,
+  ),
+);
+
+const DynamicCompareAirpodsSection = dynamic(() =>
+  import("@/_ui/sections/CompareAripods").then(
+    (component) => component.CompareAirpodsSection,
+  ),
+);
 
 export default function Home() {
   return (
@@ -10,9 +22,9 @@ export default function Home() {
 
       <HeroSection />
 
-      <AirpodsMaxSection />
+      <DynamicAirpodsMaxSection />
 
-      <CompareAirpodsSection />
+      <DynamicCompareAirpodsSection />
     </>
   );
 }
